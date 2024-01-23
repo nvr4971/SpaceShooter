18e9c816033aafa0e676bd3502a135dbb54147b3
NvR <tai.nguyennvrforget@hcmut.edu.vn>
2024-01-23 11:07:04 +0700

    Bug fixes

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -6,6 +6,8 @@ public class PlayerCollisionHandler : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log(collision.gameObject.name);
+
         if (collision.gameObject.CompareTag("Enemy"))
         {
             GetComponent<HealthStat>().TakeDamage(1);
--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -12,14 +12,28 @@ public class PlayerMovementController : MonoBehaviour
 
     private bool lockControl;
 
+    private Vector2 cameraBounds;
+    private float playerSpriteWidth;
+    private float playerSpriteHeight;
+
     private void Start()
     {
         GetComponent<SpriteRenderer>().sprite = SpriteAssets.Instance.GetPlayerSprite();
 
+        cameraBounds = Camera.main.ScreenToWorldPoint(
+            new Vector3(
+                Screen.width,
+                Screen.height,
+                Camera.main.transform.position.z
+            )
+        );
+
+        playerSpriteWidth = GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2f;
+        playerSpriteHeight = GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2f;
+
         rb = GetComponent<Rigidbody2D>();
 
         StartCoroutine(PlayerSpawnIn());
-
     }
 
     IEnumerator PlayerSpawnIn()
@@ -28,7 +42,7 @@ public class PlayerMovementController : MonoBehaviour
         vertical = 1;
         horizontal = 0;
 
-        yield return new WaitForSeconds(0.8f);
+        yield return new WaitForSeconds(0.3f);
 
         lockControl = false;
 
@@ -37,25 +51,34 @@ public class PlayerMovementController : MonoBehaviour
 
     private void Update()
     {
-        PlayerMove();
-    }
-
-    private void PlayerMove()
-    {
         if (!lockControl)
         {
-            horizontal = Input.GetAxisRaw("Horizontal");
-            vertical = Input.GetAxisRaw("Vertical");
-
-            Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
-            pos.x = Mathf.Clamp(pos.x, 0.05f, 0.95f);
-            pos.y = Mathf.Clamp(pos.y, 0.05f, 0.95f);
-            transform.position = Camera.main.ViewportToWorldPoint(pos);
+            PlayerMove();
         }
+        
+    }
+
+    private void LateUpdate()
+    {
+        PlayerBounds();
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + (playerMoveSpeed * Time.fixedDeltaTime * new Vector2(horizontal, vertical).normalized));
+    }
+
+    private void PlayerMove()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void PlayerBounds()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, cameraBounds.x * -1 + playerSpriteWidth, cameraBounds.x - playerSpriteWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, cameraBounds.y * -1 + playerSpriteHeight, cameraBounds.y - playerSpriteHeight);
+        transform.position = viewPos;
     }
 }
